1c4bf92e9492be6d06eca53a01422b8461bcb5bf
Armen Arsakian <arsakian@digitalforensics.gr>
2024-09-03 13:11:20 +0300

    show info about tape file

--- reader.go
+++ reader.go
@@ -13,6 +13,7 @@ import (
 
 type Media_Header struct {
 	Tape     *dblk.MTF_Tape
+	Info     map[string]string
 	FileMark *dblk.MTF_SFMB
 }
 
@@ -22,21 +23,56 @@ type DataSet struct {
 	Data_stream     *dblk.DATA_STREAM
 	Pad_stream      *dblk.PAD_STREAM
 	MTF_VOLB        *dblk.MTF_VOLB
+	Info            map[string]string
 }
 
-func (dataset *DataSet) AppendData(data []byte) {
-	dataset.Data_stream.AppendData(data)
+func (dataset *DataSet) AppendData(data []byte) int64 {
+	return dataset.Data_stream.AppendData(data)
+}
+
+func (dataset DataSet) IsFull() bool {
+	return dataset.Data_stream.IsFull()
+}
+
+func (dataset DataSet) Export() {
+	var err error
+	var fhandler *os.File
+	nofBytesWritten := 0
+	fhandler, err = os.Create(dataset.Info["DataSetName"])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	nofBytesWritten, err = fhandler.Write(dataset.Data_stream.Data.Bytes())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Written %d\n", nofBytesWritten)
+}
+
+func (media_header Media_Header) showInfo() {
+	fmt.Printf("Software Name: %s | Media Name: %s | Media Description: %s | Media Date %s\n",
+		media_header.Info["SoftwareName"], media_header.Info["MediaName"], media_header.Info["MediaDescription"],
+		media_header.Info["MediaData"])
+}
+
+func (data_set DataSet) showInfo() {
+	fmt.Printf("Data Set Name: %s | Data Set Description %s | Username %s Write Date %s\n",
+		data_set.Info["DataSetName"], data_set.Info["DataSetDescription"],
+		data_set.Info["UserName"], data_set.Info["MediaWriteDate"])
 }
 
 func main() {
 	filePath := flag.String("mtf", "", "path to microsoft tape archive")
 	loggerActive := flag.Bool("log", false, "enable logging")
+	info := flag.Bool("info", false, "show info about the tape file")
+	export := flag.Bool("export", false, "export data set of the tape file")
+
+	flag.Parse()
 
 	now := time.Now()
 	logfilename := "logs" + now.Format("2006-01-02T15_04_05") + ".txt"
 	logger.InitializeLogger(*loggerActive, logfilename)
-
-	flag.Parse()
 
 	fhadler, err := os.Open(*filePath)
 	if err != nil {
@@ -46,128 +82,142 @@ func main() {
 
 	offset := int64(0)
 
-	buffer := make([]byte, 100*1024)
+	buffer := make([]byte, 100000*1024)
+
+	fsize, err := fhadler.Stat()
+	if err != nil {
+		logger.MTFlogger.Error(err)
+
+	}
 
 	var media_header Media_Header
 	var data_set DataSet
 
 	latest_attribute := ""
-	for {
+	for offset < fsize.Size() {
 		_, err = fhadler.ReadAt(buffer, offset)
 		if err != nil {
-			log.Fatal(err)
+			logger.MTFlogger.Error(err)
 			break
 		}
 
-		if latest_attribute == "MQDA" { //continue
-			data_set.AppendData(buffer)
-			offset += int64(len(buffer))
-		}
-
-		for offset < int64(len(buffer)) {
-
-			if string(buffer[offset:offset+4]) == "TAPE" {
+		innerOffset := int64(0)
+		for innerOffset < int64(len(buffer)) {
+			if string(buffer[innerOffset:innerOffset+4]) == "TAPE" {
 
 				mtf_tape := new(dblk.MTF_Tape)
-				next_offset, err := mtf_tape.Parse(buffer[offset:])
+				next_offset, err := mtf_tape.Parse(buffer[innerOffset:])
 
+				media_header.Info = mtf_tape.GetInfo(buffer[innerOffset:])
 				media_header.Tape = mtf_tape
-				offset += next_offset
+				innerOffset += next_offset
 				if err != nil {
-
 					break
 				}
 
-			} else if string(buffer[offset:offset+4]) == "SFMB" {
+			} else if string(buffer[innerOffset:innerOffset+4]) == "SFMB" {
 
 				mtf_sfmb := new(dblk.MTF_SFMB)
-				next_offset, err := mtf_sfmb.Parse(buffer[offset:])
+				next_offset, err := mtf_sfmb.Parse(buffer[innerOffset:])
 
 				media_header.FileMark = mtf_sfmb
-				offset += next_offset
+				innerOffset += next_offset
 				if err != nil {
 					break
 				}
 
-			} else if string(buffer[offset:offset+4]) == "SSET" {
+			} else if string(buffer[innerOffset:innerOffset+4]) == "SSET" {
 
 				mtf_sset := new(dblk.MTF_SSET)
-				next_offset, err := mtf_sset.Parse(buffer[offset:])
+				next_offset, err := mtf_sset.Parse(buffer[innerOffset:])
 
+				data_set.Info = mtf_sset.GetInfo(buffer[innerOffset:])
 				data_set.MTF_SSET = mtf_sset
-				offset += next_offset
+				innerOffset += next_offset
 				if err != nil {
 					break
 				}
 
-			} else if string(buffer[offset:offset+4]) == "VOLB" {
+			} else if string(buffer[innerOffset:innerOffset+4]) == "VOLB" {
 
 				mtf_volb := new(dblk.MTF_VOLB)
-				next_offset, err := mtf_volb.Parse(buffer[offset:])
+				next_offset, err := mtf_volb.Parse(buffer[innerOffset:])
 
 				data_set.MTF_VOLB = mtf_volb
-				offset += next_offset
+				innerOffset += next_offset
 				if err != nil {
 					break
 				}
 
-			} else if string(buffer[offset:offset+4]) == "SPAD" {
+			} else if string(buffer[innerOffset:innerOffset+4]) == "SPAD" {
 				pad_stream := new(dblk.PAD_STREAM)
-				next_offset, err := pad_stream.Parse(buffer[offset:])
+				next_offset, err := pad_stream.Parse(buffer[innerOffset:])
 
 				data_set.Pad_stream = pad_stream
-				offset += next_offset
+				innerOffset += next_offset
 				if err != nil {
 					break
 				}
 
-			} else if string(buffer[offset:offset+4]) == "RAID" {
+			} else if string(buffer[innerOffset:innerOffset+4]) == "RAID" {
 				raid_stream := new(dblk.RAID_STREAM)
-				next_offset, err := raid_stream.Parse(buffer[offset:])
+				next_offset, err := raid_stream.Parse(buffer[innerOffset:])
 
-				offset += next_offset
+				innerOffset += next_offset
 				if err != nil {
 					break
 				}
 
-			} else if string(buffer[offset:offset+4]) == "MSCI" || string(buffer[offset:offset+4]) == "MSDA" {
+			} else if string(buffer[innerOffset:innerOffset+4]) == "MSCI" || string(buffer[innerOffset:innerOffset+4]) == "MSDA" {
 				mtf_generic := new(dblk.MTF_Generic)
-				next_offset, err := mtf_generic.Parse(buffer[offset:])
+				next_offset, err := mtf_generic.Parse(buffer[innerOffset:])
 
-				offset += next_offset
+				innerOffset += next_offset
 				if err != nil {
-
 					break
 				}
 
-			} else if string(buffer[offset:offset+4]) == "MQCI" || string(buffer[offset:offset+4]) == "APAD" ||
-				string(buffer[offset:offset+4]) == "CSUM" {
+			} else if string(buffer[innerOffset:innerOffset+4]) == "MQCI" || string(buffer[innerOffset:innerOffset+4]) == "APAD" ||
+				string(buffer[innerOffset:innerOffset+4]) == "CSUM" {
 				generic_stream := new(dblk.GENERIC_STREAM)
-				next_offset, err := generic_stream.Parse(buffer[offset:])
+				next_offset, err := generic_stream.Parse(buffer[innerOffset:])
 
 				data_set.Generic_streams = append(data_set.Generic_streams, generic_stream)
-				offset += next_offset
+				innerOffset += next_offset
 				if err != nil {
 					break
 				}
 
-			} else if string(buffer[offset:offset+4]) == "MQDA" {
+			} else if string(buffer[innerOffset:innerOffset+4]) == "MQDA" {
 				data_stream := new(dblk.DATA_STREAM)
-				next_offset, err := data_stream.Parse(buffer[offset:])
+				next_offset, err := data_stream.Parse(buffer[innerOffset:])
 
 				data_set.Data_stream = data_stream
-				offset += next_offset
+				innerOffset += next_offset
 				if err != nil {
 					latest_attribute = "MQDA"
 					break
 				}
+			} else if latest_attribute == "MQDA" && !data_set.IsFull() { //break
+				innerOffset += data_set.AppendData(buffer)
+
 			} else {
-				offset += 1 //brute force search alignment??
+				innerOffset += 1 //brute force search alignment??
 				logger.MTFlogger.Warning(fmt.Sprintf("Bruteforcing %d", offset))
 			}
-
 		}
+		offset += innerOffset
 
+	}
+
+	if *info {
+
+		media_header.showInfo()
+		data_set.showInfo()
+	}
+
+	if *export {
+		data_set.Export()
 	}
 
 }
